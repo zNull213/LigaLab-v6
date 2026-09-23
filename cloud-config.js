da645a9271a94cc9cfa1b8bbafaddebccb6cc4b2
zNull213 <znull785@gmail.com>
2026-09-22 23:18:45 -0300

    Add files via upload

---
+++ cloud-config.js
@@ -0,0 +1,9 @@
+/*
+  LigaLab WEB v6.2
+  Estos dos valores son PUBLICABLES en una app web siempre que RLS esté activado.
+  NUNCA pegues aquí una service_role key.
+*/
+window.LIGALAB_CLOUD_CONFIG = {
+  supabaseUrl: "https://iknzlffbzluodangsqny.supabase.co"
+  supabaseAnonKey: "sb_publishable_didJ9wFQDE-FdgzrccWkiA_r0sFRQas"
+};
